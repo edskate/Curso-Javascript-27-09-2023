d1ad50d232429ab33e1a6139f7f7712d724c1e14
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-27 16:27:57 -0300

    variáveis com nomes

--- 4_Variáveis/js/scripts.js
+++ 4_Variáveis/js/scripts.js
@@ -10,3 +10,37 @@ teste = "edy";
 console.log(teste);
 
 console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD@';
+
+console.log($nome);
+console.log("_nome");
+
+var nome5 = 'ASD3';
+
+console.log('nome5');
+
+var meuPrimeiroNome = 'Edy';
+
+console.log(meuPrimeiroNome);
+
+var  meusobrenome = 'lima';
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+
+meuNome = 'edy';
+
+
+console.log(meuNome);
