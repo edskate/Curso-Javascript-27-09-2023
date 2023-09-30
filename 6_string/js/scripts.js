b621aadb6465cff996f25443ec5f1524a4eba4dc
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-30 19:57:45 -0300

    add  script

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,21 @@
+var nome = "Edilson";
+var sobrenome= "lima";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+var  nome = "edilson";
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = "está é uma frase completa";
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " "  + frase);
