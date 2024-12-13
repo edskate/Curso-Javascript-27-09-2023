3e670e8d70e59cbd791d42d5532604685873f367
edilson gomes de lima <edskateicevida@gmail.com>
2024-12-12 22:49:32 -0300

    atualização values

---
+++ Empety Values/scripts.js
@@ -0,0 +1,13 @@
+// 12 Empty Value
+
+console.log(typeof null, typeof undefined); // object undefined
+
+console.log(null === undefined); // false 
+
+console.log(null == undefined); // true
+
+console.log(null == false); // false
+
+console.log(undefined == false); // true
+
+
